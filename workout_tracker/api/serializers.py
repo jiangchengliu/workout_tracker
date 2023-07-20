3b4858665ab9718f95aaf79b602caec3176a7446
Jiang Cheng Liu <jiangcl@bu.edu>
2023-07-20 01:54:34 -0400

    feat(serializers): add new serializers for Exercise and WorkoutSession models

--- workout_tracker/api/serializers.py
+++ workout_tracker/api/serializers.py
@@ -1,2 +1,17 @@
 from rest_framework import serializers
+from api.models import *
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = '__all__'
+
+class WorkoutSessionSerializer(serializers.ModelSerializer):
+    exercises = ExerciseSerializer(many=True, read_only=True)
+    class Meta:
+        model = WorkoutSession
+        fields = '__all__'
+
+
+
 
--- workout_tracker/api/views.py
+++ workout_tracker/api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('Hello World')
+
+class WorkoutSessionView(generics.ListAPIView):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
+
