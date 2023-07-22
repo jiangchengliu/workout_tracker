451582fbec7788a6d4002974cda39688e4657c59
Jiang Cheng Liu <jiangcl@bu.edu>
2023-07-22 01:33:37 -0400

    feat: initialized routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import WorkoutSessionList from './components/WorkoutSessionList';
+import WorkoutSessionList from './components/workoutSessionList/WorkoutSessionList';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <h1>Workout Tracker</h1>
-      <WorkoutSessionList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/workouts">History</Link>
+            </li>
+            <li>
+              <Link to="/create">Create Workout Session</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/workouts" element={<WorkoutSessionList />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
