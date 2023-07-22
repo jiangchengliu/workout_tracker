383f9ae04dd172790b84e43c196bc2908650a078
Jiang Cheng Liu <jiangcl@bu.edu>
2023-07-22 01:31:37 -0400

    chore: added workoutsessionlist to new folder

--- frontend/src/components/workoutSessionList/WorkoutSessionList.js
+++ frontend/src/components/workoutSessionList/WorkoutSessionList.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react'
 function WorkoutSessionList() {
     const [workoutSessions, setWorkoutSessions] = useState([])
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/api')  // Replace with your actual API endpoint
+        fetch('http://127.0.0.1:8000/api')  
             .then(response => response.json())
             .then(data => setWorkoutSessions(data));
     }, []);
@@ -11,16 +11,21 @@ function WorkoutSessionList() {
     return (
         workoutSessions.map(session => (
             <div key={session.id}>
-                <h2>{session.date}, {session.duration}</h2>
+                <h3>Workout: {session.id} </h3>
+                <p>{session.date}, {session.duration}</p>
                 <p>{session.notes}</p>
                 <h3>Exercises: </h3>
                     {session.exercises.map(exercise => (
                         <div key={exercise.id}>
                             <p>Name: {exercise.name}</p>
-                            <p>Sets: {exercise.sets}</p>
-                            <p>Reps: {exercise.reps}</p>
-                            <p>Rpe: {exercise.rpe}</p>
-                            <p>Weight: {exercise.weight}</p>
+                            {exercise.set.map((s,i) => (
+                                <div key={s.id}>
+                                    <p>Set: {i + 1}</p>
+                                    <p>Reps: {s.reps}</p>
+                                    <p>Rpe: {s.rpe}</p>
+                                    <p>Weight: {s.weight}</p>
+                                </div>
+                            ))}
                         </div>
                     ))}
             </div>
