13196f428729266f2b27d8c32dbad985832c74b7
Jiang Cheng Liu <jiangcl@bu.edu>
2023-08-15 01:33:41 -0400

    chore: changed urls to match routers from drf

--- frontend/src/components/workoutSessionList/WorkoutSessionList.js
+++ frontend/src/components/workoutSessionList/WorkoutSessionList.js
@@ -1,56 +1,68 @@
-import React, {useEffect, useState} from 'react'
-import { Card, CardHeader, CardContent, Typography, Button } from '@mui/material';
-
+import React, { useEffect, useState } from 'react'
+import { Card, CardHeader, CardContent, Typography, IconButton } from '@mui/material';
+import { Delete as DeleteIcon } from '@mui/icons-material';
 
 function WorkoutSessionList() {
 
     const [workoutSessions, setWorkoutSessions] = useState([])
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/WorkoutHistory')  
+        fetch('http://127.0.0.1:8000/workouts/')
             .then(response => response.json())
             .then(data => setWorkoutSessions(data));
     }, []);
 
     const deleteWorkoutSession = (id) => {
-        fetch(`http://127.0.0.1:8000/WorkoutHistoryDelete/${id}`, {
+
+        fetch(`http://127.0.0.1:8000/workouts/${id}/`, {
             method: 'DELETE',
         })
-        .then(response => {
-            if (response.ok) {
-                setWorkoutSessions(workoutSessions.filter(session => session.id != id))
-                console.log("Deleted!")
-            } else {
-                throw new Error('Could not delete');
-            }
-        })
-        .catch(error => console.log(error));    
+            .then(response => {
+                if (response.ok) {
+                    setWorkoutSessions(workoutSessions.filter(session => session.id != id))
+                    console.log("Deleted!")
+                } else {
+                    throw new Error('Could not delete');
+                }
+            })
+            .catch(error => console.log(error));
     }
 
 
     return (
-
         workoutSessions.map(session => (
-            <Card key={session.id} sx={{ margin: 'auto', marginBottom: '16px', maxWidth: '400px', marginTop: '16px'}}>
-                <CardHeader title={'Workout'} sx={{ textAlign: 'center' }} />
-                <CardContent sx={{ textAlign: 'center' }}>
-                    <Typography>{`${session.date}, ${session.duration}`}</Typography>
-                    <Typography>{session.notes}</Typography>
+            <Card key={session.id} sx={{ margin: 'auto', marginBottom: '16px', maxWidth: '400px', marginTop: '16px' }}>
+                <CardHeader
+                    title="Workout"
+                    subheader={
+                        <Typography
+                            variant="body2"
+                            sx={{
+                                fontStyle: 'italic',
+                                textAlign: 'left'
+                            }}>
+                            {`${session.date} | ${session.duration}`}
+                        </Typography>
+                    }
+                    action={
+                        <IconButton variant="contained" color="error" aria-label="delete-button" onClick={() => deleteWorkoutSession(session.id)}>
+                            <DeleteIcon />
+                        </IconButton>
+                    }
+                />
+                <CardContent sx={{ textAlign: 'left' }}>
                     <Typography variant="h6">Exercises:</Typography>
                     {session.exercises.map(exercise => (
                         <div key={exercise.id} sx={{ marginLeft: '16px' }}>
-                            <Typography variant="subtitle1">{`Name: ${exercise.name}`}</Typography>
-                            {exercise.set.map((s, i) => (
+                            <Typography variant="subtitle1">{`${exercise.name}`}</Typography>
+                            {exercise.sets.map((s, i) => (
                                 <div key={s.id} sx={{ marginLeft: '16px' }}>
-                                    <Typography>{`Set: ${i + 1}`}</Typography>
-                                    <Typography>{`Reps: ${s.reps}`}</Typography>
-                                    <Typography>{`Rpe: ${s.rpe}`}</Typography>
-                                    <Typography>{`Weight: ${s.weight}`}</Typography>
+                                    <Typography>{`${i + 1} sets x ${s.reps} reps x ${s.weight} lbx @ rpe ${s.rpe}`}</Typography>
                                 </div>
                             ))}
                         </div>
                     ))}
-                    <Button variant="contained" color="error" onClick={() => deleteWorkoutSession(session.id)}>Delete</Button>
+                    <Typography>{session.notes}</Typography>
                 </CardContent>
             </Card>
         ))
