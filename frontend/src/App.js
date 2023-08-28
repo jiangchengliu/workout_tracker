5735fd7123b584e591b8f6314aade635e36faa0a
Jiang Cheng Liu <jiangcl@bu.edu>
2023-08-28 01:56:37 -0400

    fix: navigation bar and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import NavBar from './components/nBar/NBar';
+import { BrowserRouter as Router, Routes, Route, Redirect } from "react-router-dom";
+import WorkoutSessionList from './components/workoutSessionList/WorkoutSessionList'
+import ExerciseList from './components/exerciseList/ExerciseList';
+import ExerciseDetail from './components/exerciseList/ExerciseDetails';
+import CreateWorkout from './components/newWorkout/newWorkout';
+import Login from './components/login';
+import SignUp from './components/signup';
+import PrivateRoute from './utils/privateRoute';
+import { AuthProvider } from './context/authContext';
+
 
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <Router>
+      <AuthProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/workouts" element={<PrivateRoute><WorkoutSessionList /></PrivateRoute>} />
+          <Route path="/exercises" element={<PrivateRoute><ExerciseList /></PrivateRoute>} />
+          <Route path="/exercises/:id" element={<PrivateRoute><ExerciseDetail /></PrivateRoute>} />
+          <Route path="/create" element={<PrivateRoute><CreateWorkout /></PrivateRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<SignUp />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
--- frontend/src/components/nBar/NBar.js
+++ frontend/src/components/nBar/NBar.js
@@ -1,37 +1,37 @@
 import { AppBar, IconButton, Toolbar, Typography, Stack, Button } from '@mui/material'
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import { BrowserRouter as Router, Routes, Route, Link as RouterLink } from "react-router-dom";
-import WorkoutSessionList from '../workoutSessionList/WorkoutSessionList';
-import ExerciseList from '../exerciseList/ExerciseList';
-import ExerciseDetail from '../exerciseList/ExerciseDetails';
-import CreateWorkout from '../newWorkout/newWorkout';
+import { useState, useEffect, useContext } from 'react';
+import AuthContext from '../../context/authContext';
+
 
 
 function NavBar() {
+
+    const { user, logoutUser } = useContext(AuthContext);
+
+
     return (
-        <Router>
-            <AppBar position="sticky">
-                <Toolbar>
-                    <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
-                        <FitnessCenterIcon />
-                    </IconButton>
-                    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-                        Workout Tracker
-                    </Typography>
-                    <Stack direction='row' spacing={2}>
+        <AppBar position="sticky">
+            <Toolbar>
+                <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+                    <FitnessCenterIcon />
+                </IconButton>
+                <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+                    Workout Tracker
+                </Typography>
+                {user ? (
+                    <>
                         <Button color='inherit' component={RouterLink} to="/workouts">History</Button>
                         <Button color='inherit' component={RouterLink} to="/create">New Workout</Button>
                         <Button color='inherit' component={RouterLink} to="/exercises">Exercises</Button>
-                    </Stack>
-                </Toolbar>
-            </AppBar>
-            <Routes>
-                <Route path="/workouts" element={<WorkoutSessionList />} />
-                <Route path="/exercises" element={<ExerciseList />} />
-                <Route path="/exercises/:id" element={<ExerciseDetail />} />
-                <Route path="/create" element={<CreateWorkout />} />
-            </Routes>
-        </Router>
+                        <Button color='inherit' onClick={logoutUser}>Logout</Button>
+                    </>
+                ) : (
+                    <Button color='inherit' component={RouterLink} to="/login">Login</Button>
+                )}
+            </Toolbar>
+        </AppBar>
     );
 }
 
